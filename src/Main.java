843b6f76d339f215cc4ad9408d706b6d1910d0c5
ThomasLuuu <luutoan07@gmail.com>
2022-03-24 23:33:40 +0700

    Adding many classes
    I have add classes to check files, string, split to add and List check!!!

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main implements StudentsEnrolmentManager {
     static Scanner obj = new Scanner(System.in);
     static ArrayList<Courses> coursesList = new ArrayList();
     static ArrayList<Students> studentsList = new ArrayList();
     static ArrayList<StudentsEnrolment> studentsEnrolmentList = new ArrayList<>();
-
     static String[] semester = {"2020A", "2020B", "2020C","2021A","2021B","2021C","2022A","2022B","2022C","2023A","2023B","2023C","2024A","2024B","2024C"};
     static ArrayList semesters = new ArrayList(Arrays.asList(semester));
+
+    ArrayList<String> saveInfo = new ArrayList<String>();
 
     public static void welcome(){
         System.out.println("welcome to Student Management Program");
         System.out.println("please choose the section you want to do");
-        System.out.println("1: display student information");
+        System.out.println("1: display information");
         System.out.println("2: add student information");
         System.out.println("3: delete student information");
         System.out.println("4: update student information");
@@ -31,18 +27,6 @@ public class Main implements StudentsEnrolmentManager {
         return option;
     }
 
-//    public static void readFile(){
-//        try {
-//            Scanner sc = new Scanner(new File("src//default.csv"));
-//            sc.useDelimiter(", ");
-//            while(sc.hasNext()){
-//                System.out.print(sc.next());
-//            }
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//
-//    }
 
     public static String readFile(){
         boolean done = false;
@@ -93,11 +77,72 @@ public class Main implements StudentsEnrolmentManager {
         }
         return fileName;
     }
+    private static void saveFile(String type, String fileName, List data) throws IOException {
+        String name = "src/"+fileName+".csv";
+        File csvFile = new File(name);
+        FileWriter fileWriter = new FileWriter(csvFile, true);
+        List<List<String>> al = new ArrayList<>();
+        al = data;
+        switch (type){
+            case"student" :{
+                fileWriter.write("Course");
+                fileWriter.append(',');
+                fileWriter.write("Course id");
+                fileWriter.write("\n");
+                for (int i=0; i<al.size();i++){
+                    fileWriter.write((al.get(i)).get(0).split(",")[0]);
+                    fileWriter.append(',');
+                    fileWriter.write((al.get(i)).get(0).split(",")[1]);
+                    fileWriter.write("\n");
+                }
+                break;
+            }
+            case"course":{
+                fileWriter.write("Student");
+                fileWriter.append(',');
+                fileWriter.write("Student id");
+                fileWriter.write("\n");
+                for (int i=0; i<al.size();i++){
+                    fileWriter.write((al.get(i)).get(0).split(",")[0]);
+                    fileWriter.append(',');
+                    fileWriter.write((al.get(i)).get(0).split(",")[1]);
+                    fileWriter.write("\n");
+                }
+                break;
+            }
+            case"sem":{
+                fileWriter.write("Course");
+                fileWriter.append(',');
+                fileWriter.write("Course id");
+                fileWriter.write("\n");
+                for (int i=0; i<al.size();i++){
+                    fileWriter.write((al.get(i)).get(0).split(",")[0]);
+                    fileWriter.append(',');
+                    fileWriter.write((al.get(i)).get(0).split(",")[1]);
+                    fileWriter.write("\n");
+                }
+                break;
+            }
+        }
 
 
+        String re = (al.get(0)).get(0).split(",")[0];
+        String re1 = (al.get(0)).get(0).split(",")[1];
+        System.out.println(re);
+        System.out.println(re1);
+        fileWriter.flush();
+        fileWriter.close();
 
 
-
+    }
+    public boolean checkFileExist(String fileName){
+        boolean check = false;
+        File tempFile =   new File("src/"+fileName+".csv");
+        if(tempFile.exists()){
+            check = true;
+        }
+        return check;
+    }
 
     public void Update(){
 
@@ -105,22 +150,205 @@ public class Main implements StudentsEnrolmentManager {
     public void Delete(){
 
     }
-    public void GetOne(){
-        boolean flag = false;
-//        Scanner obj1 = new Scanner(System.in);
-//        System.out.println("Give me the ID you want to find: ");
-//        String findId = obj1.nextLine();
-        for (StudentsEnrolment std:studentsEnrolmentList){
-            if(std.students.getId() == "1223") {
-                flag = true;
+    public void Display(String type){
+        String displayType = type;
+        switch (displayType){
+            case "students":{
+                System.out.println("Here is the list of students: ");
+                for(StudentsEnrolment studentsEnrolment: studentsEnrolmentList){
+                    System.out.println(studentsEnrolment.getStudents().getId() + " || " + studentsEnrolment.getStudents().getName());
+                }
+                break;
             }
-            else{ flag=false;}
+            case "courses":{
+                System.out.println("Here is the list of courses: ");
+                for (StudentsEnrolment studentsEnrolment: studentsEnrolmentList){
+                    System.out.println(studentsEnrolment.getCourses().getCourseId() + " || " + studentsEnrolment.getCourses().getCourseName());
+                }
+                break;
+            }
+            case "sem":{
+                System.out.println("Here is the list of semester: ");
+                for( String sem: semester){
+                    System.out.println(sem);
+                }
+                break;
+            }
         }
-        if(flag){
-            System.out.println("The list does not have that student you input");
-        }else {
-            System.out.println("!");
+    }
+    public void GetOne() throws IOException {
+        boolean flag =false;
+        Scanner obj2 = new Scanner(System.in);
+        System.out.println("Do you want to " +
+                "[1] Print all courses for 1 student in 1 semester. " +
+                "[2] Print all students of 1 course in 1 semester. " +
+                "[3] Prints all courses offered in 1 semester");
+        String result = obj2.nextLine();
+        List<List<String>> al = new ArrayList<>();
+        switch (result){
+            case "1":{
+                Display("students");
+                System.out.println("Please enter Sid you want to print all courses ");
+                String studentSearch = obj2.nextLine();
+                for(StudentsEnrolment studentsEnrolment: studentsEnrolmentList){
+                    if(studentsEnrolment.getStudents().getId().equals(studentSearch)){
+                        String dataInput = studentsEnrolment.getCourses().getCourseName() + ", " + studentsEnrolment.getCourses().getCourseId();
+                        System.out.println("This student has enrolled " + dataInput);
+//                        saveInfo.add(dataInput);
+
+                        al.add(Arrays.asList(dataInput));
+                        flag = true;
+                    }
+                }
+
+                if (!flag){
+                    System.out.println("Your input is invalid");
+                    break;
+                }
+                System.out.println("Do you want to save file?" +
+                        "[1] Yes" +
+                        "[2] No");
+                String choice =obj2.nextLine();
+                switch (choice){
+                    case "1":{
+                        System.out.println("Type the file name you want to save: ");
+                        String csvFileName = obj2.nextLine();
+                        if(checkFileExist(csvFileName)){
+                            System.out.println("This file is exit in the system, do you want to override it? " +
+                                    "[1] YES" +
+                                    "[2] NO");
+                            String overRideChoice = obj2.nextLine();
+                            switch (overRideChoice){
+                                case "1":{
+                                    saveFile("student",csvFileName, al);
+                                    System.out.println("Save Successfully Override to the file: " + csvFileName +".csv");
+                                    break;
+                                }
+                                case "2":{
+                                    System.exit(0);
+                                    break;
+                                }
+
+                            }
+                        }else {
+                            saveFile("student",csvFileName, al);
+                            System.out.println("Save Successfully to the file: " + csvFileName +".csv");
+                        }
+
+                    }
+                }
+
+
+                break;
+            }
+            case "2": {
+                Display("courses");
+                System.out.println("Please enter Course code you want to print all students enrolled: ");
+                String courseSearch = obj2.nextLine();
+                for(StudentsEnrolment studentsEnrolment: studentsEnrolmentList){
+                    if(studentsEnrolment.getCourses().getCourseId().equals(courseSearch)){
+                        String dataInput = studentsEnrolment.getStudents().getName() + ", " + studentsEnrolment.getStudents().getId();
+                        System.out.println("Student enrolled: " + dataInput);
+                        flag = true;
+                        al.add(Arrays.asList(dataInput));
+
+                    }
+                }
+
+                if (!flag){
+                    System.out.println("Your input is invalid");
+                    System.exit(0);
+                }
+                System.out.println("Do you want to save file?" +
+                        "[1] Yes" +
+                        "[2] No");
+                String choice =obj2.nextLine();
+                switch (choice){
+                    case "1":{
+                        System.out.println("Type the file name you want to save: ");
+                        String csvFileName = obj2.nextLine();
+                        if(checkFileExist(csvFileName)){
+                            System.out.println("This file is exit in the system, do you want to override it? " +
+                                    "[1] YES" +
+                                    "[2] NO");
+                            String overRideChoice = obj2.nextLine();
+                            switch (overRideChoice){
+                                case "1":{
+                                    saveFile("student",csvFileName, al);
+                                    System.out.println("Save Successfully Override to the file: " + csvFileName +".csv");
+                                    break;
+                                }
+                                case "2":{
+                                    System.exit(0);
+                                    break;
+                                }
+
+                            }
+                        }else {
+                            saveFile("course",csvFileName, al);
+                            System.out.println("Save Successfully to the file: " + csvFileName +".csv");
+                        }
+
+                    }
+                }
+                break;
+            }
+            case "3":{
+                Display("sem");
+                System.out.println("Please enter Semester you want to print all courses: ");
+                String semSearch = obj2.nextLine();
+                for (StudentsEnrolment studentsEnrolment: studentsEnrolmentList){
+                    if(studentsEnrolment.getSemester().equals(semSearch)){
+                        String dataInput = studentsEnrolment.getCourses().getCourseId() + ", " +studentsEnrolment.getCourses().getCourseName();
+                        System.out.println("Courses in this sem: "+ dataInput);
+                        al.add(Arrays.asList(dataInput));
+                        flag = true;
+                    }
+                }
+
+                if(!flag){
+                    System.out.println("Your input is invalid");
+                    System.exit(0);
+                }
+                System.out.println("Do you want to save file?" +
+                        "[1] Yes" +
+                        "[2] No");
+                String choice =obj2.nextLine();
+                switch (choice){
+                    case "1":{
+                        System.out.println("Type the file name you want to save: ");
+                        String csvFileName = obj2.nextLine();
+                        if(checkFileExist(csvFileName)){
+                            System.out.println("This file is exit in the system, do you want to override it? " +
+                                    "[1] YES" +
+                                    "[2] NO");
+                            String overRideChoice = obj2.nextLine();
+                            switch (overRideChoice){
+                                case "1":{
+                                    saveFile("student",csvFileName, al);
+                                    System.out.println("Save Successfully Override to the file: " + csvFileName +".csv");
+                                    break;
+                                }
+                                case "2":{
+                                    System.exit(0);
+                                    break;
+                                }
+
+                            }
+                        }else {
+                            saveFile("sem",csvFileName, al);
+                            System.out.println("Save Successfully to the file: " + csvFileName +".csv");
+                        }
+
+                    }
+                }
+                break;
+            }
         }
+
+
+
+
     }
     public void GetAll(){
     if(studentsEnrolmentList.isEmpty()){
@@ -130,7 +358,7 @@ public class Main implements StudentsEnrolmentManager {
     System.out.println(studentsEnrolmentList.toString());
     }
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
         String defaultAction = readFile();
         int option;
@@ -140,7 +368,7 @@ public class Main implements StudentsEnrolmentManager {
             option = option();
             switch (option){
                 case 1: {
-                    System.out.println("Do you want to display all students ( type 1 ) or specific student( type 2): ");
+                    System.out.println("Do you want to display all students ( type 1 ) or specific information( type 2): ");
                     Scanner obj = new Scanner(System.in);
                     int choice = obj.nextInt();
                     switch (choice){
@@ -150,8 +378,8 @@ public class Main implements StudentsEnrolmentManager {
                         }
                         case 2:{
                             system.GetOne();
-                            break;
                         }
+
 
                     }
                     break;
@@ -176,6 +404,22 @@ public class Main implements StudentsEnrolmentManager {
 
 
     }
+
+    public static boolean verification(String sID, String Course, String sem){
+        boolean check = false;
+        for(StudentsEnrolment studentsEnrolment: studentsEnrolmentList){
+            if(studentsEnrolment.getStudents().getId().equals(sID)){
+                if(studentsEnrolment.getCourses().getCourseId().equals(Course)){
+                    if (semesters.contains(sem)){
+                        check = false;
+                    }
+                }
+            }
+
+        }
+        System.out.println(check);
+        return check;
+    }
     public static void Add(){
         Students result1 = null;
         Courses result2 = null;
@@ -191,11 +435,11 @@ public class Main implements StudentsEnrolmentManager {
             if (student.getId().equals(studentID))
                 result1 = student;
         }
+
         if (result1 == null) {
             System.out.println("No student available with given ID.");
             return;
         }
-
         System.out.println("List of available courses:");
         for (StudentsEnrolment studentEnrolment : studentsEnrolmentList) {
             System.out.println(studentEnrolment.getCourses().getCourseId() + " " + studentEnrolment.getCourses().getCourseName() + " " + studentEnrolment.getCourses().getNumberOfCredit());
@@ -217,9 +461,8 @@ public class Main implements StudentsEnrolmentManager {
         String Sem = obj.nextLine();
         if (!(semesters.contains(Sem))) {
             System.out.println("Not an available semester.");
-            return;
         }
-
+        if(verification(studentID,courseID,Sem)){
         StudentsEnrolment record = new StudentsEnrolment(result1, result2, Sem);
         if (studentsEnrolmentList.contains(record)){
             System.out.println("Enrolment already existed");
@@ -228,7 +471,7 @@ public class Main implements StudentsEnrolmentManager {
         studentsEnrolmentList.add(record);
         System.out.println("Success");
         System.out.println(result1.getId() + " " + result1.getName() + ", " + result2.getCourseId()+ " " + result2.getCourseName() + ", " + Sem);
-    }
+    }}
 
 
 }
