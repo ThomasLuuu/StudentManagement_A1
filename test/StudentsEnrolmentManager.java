e1583e16dd2f84b2199ec8226b08e4b7fb758a4f
ThomasLuuu <luutoan07@gmail.com>
2022-04-04 17:06:02 +0700

    final 1

--- test/StudentsEnrolmentManager.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public interface StudentsEnrolmentManager {
-     void Add(String sID);
-     void Update();
-     void Delete(String sID) throws IOException;
-     void GetOne() throws IOException;
-    void GetAll();
-}
